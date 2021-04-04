50441dc213c6d284ca18fb76c2e6fc38dd536399
2hdlockness <77622028+2hdlockness@users.noreply.github.com>
2021-04-04 19:45:21 +0200

    Update MMM-IronManGIF.js

--- MMM-IronManGIF.js
+++ MMM-IronManGIF.js
@@ -8,7 +8,7 @@
 Module.register("MMM-IronManGIF", {
   // Default module config.
   defaults: {
-    style: "1",
+    style: 1,
     maxWidth: "100%"
   },
 
@@ -16,32 +16,26 @@ Module.register("MMM-IronManGIF", {
     self = this;
     this.url = "";
     this.GifUrls = {
-      "1": "./modules/MMM-IronManGIF/Resources/1.gif",
-      "2": "./modules/MMM-IronManGIF/Resources/2.gif",
-      "3": "./modules/MMM-IronManGIF/Resources/3.gif",
-      "4": "./modules/MMM-IronManGIF/Resources/4.gif",
-      "5": "./modules/MMM-IronManGIF/Resources/5.gif",
-      "6": "./modules/MMM-IronManGIF/Resources/6.gif",
-      "7": "./modules/MMM-IronManGIF/Resources/7.gif",
-      "8": "./modules/MMM-IronManGIF/Resources/8.gif",                                    
+      1: "./modules/MMM-IronManGIF/Resources/1.gif",
+      2: "./modules/MMM-IronManGIF/Resources/2.gif",
+      3: "./modules/MMM-IronManGIF/Resources/3.gif",
+      4: "./modules/MMM-IronManGIF/Resources/4.gif",
+      5: "./modules/MMM-IronManGIF/Resources/5.gif",
+      6: "./modules/MMM-IronManGIF/Resources/6.gif",
+      7: "./modules/MMM-IronManGIF/Resources/7.gif",
+      8: "./modules/MMM-IronManGIF/Resources/8.gif",                                    
     }
     
     if (this.config.style != "") {
         this.url = this.GifUrls[this.config.style];
     }
   },      
-
-  getStyles: function() {
-    return ["MMM-IronManGIF.css"];
-  },
-
+  
   getDom: function() {
     var wrapper = document.createElement("div");
     var image = document.createElement("img");
     if (this.config.style != "") {
-        image.classList.add = "photo";
         image.src = this.url ;
-
         image.style.maxWidth = this.config.maxWidth;
     }
     wrapper.appendChild(image);
@@ -50,9 +44,9 @@ Module.register("MMM-IronManGIF", {
 
   notificationReceived: function(notification, payload) {
     if (notification === 'HIDE_IronManGIF') {
-      this.hide(1000, {lockString: "LOCKEDBYMODULE"});
+      this.hide(1000, {lockString: "LOCKEDMODULE"});
     } else if (notification === 'SHOW_IronManGIF') {
-      this.show(1000, {lockString: "LOCKEDBYMODULE"});
+      this.show(1000, {lockString: "LOCKEDMODULE"});
     }
 
   }
